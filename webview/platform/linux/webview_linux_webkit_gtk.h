cf8599e452e93c6749b49a5e08d9f4534f1b734a
mid-kid <esteve.varela@gmail.com>
2021-04-28 14:25:39 +0200

    Include gdk/gdkx.h in webview_linux_webkit_gtk.h
    This fixes the following build error:
    
    webview/platform/linux/webview_linux_webkit_gtk.cpp:40:27: error: ‘::gdk_x11_window_get_xid’ has not been declared; did you mean ‘Webview::WebkitGtk::gdk_x11_window_get_xid’?
       40 |   && LOAD_GTK_SYMBOL(gtk, gdk_x11_window_get_xid)
          |                           ^~~~~~~~~~~~~~~~~~~~~~

--- webview/platform/linux/webview_linux_webkit_gtk.h
+++ webview/platform/linux/webview_linux_webkit_gtk.h
@@ -12,6 +12,7 @@ extern "C" {
 #undef signals
 #include <JavaScriptCore/JavaScript.h>
 #include <gtk/gtk.h>
+#include <gdk/gdkx.h>
 #include <webkit2/webkit2.h>
 #include <X11/Xlib.h>
 #define signals public
