d661b1badac2a2515e7a3b0b50fe5a47f55c759b
John Preston <johnprestonmail@gmail.com>
2022-07-07 08:37:34 +0400

    Support complex blocking popups.

--- webview/webview_dialog.cpp
+++ webview/webview_dialog.cpp
@@ -24,12 +24,42 @@
 #include <QtCore/QEventLoop>
 
 namespace Webview {
+namespace {
 
-DialogResult DefaultDialogHandler(DialogArgs args) {
+constexpr auto kPopupsQuicklyLimit = 3;
+constexpr auto kPopupsQuicklyDelay = 3 * crl::time(1000);
+
+bool InBlockingPopup/* = false*/;
+int PopupsShownQuickly/* = 0*/;
+crl::time PopupLastShown/* = 0*/;
+
+} // namespace
+
+PopupResult ShowBlockingPopup(PopupArgs &&args) {
+	if (InBlockingPopup) {
+		return {};
+	}
+	InBlockingPopup = true;
+	const auto guard = gsl::finally([] {
+		InBlockingPopup = false;
+	});
+
+	if (!args.ignoreFloodCheck) {
+		const auto now = crl::now();
+		if (!PopupLastShown || PopupLastShown + kPopupsQuicklyDelay <= now) {
+			PopupsShownQuickly = 1;
+		} else if (++PopupsShownQuickly > kPopupsQuicklyLimit) {
+			return {};
+		}
+	}
+	const auto timeguard = gsl::finally([] {
+		PopupLastShown = crl::now();
+	});
+
 	// This fixes animations in a nested event loop.
 	base::Integration::Instance().enterFromEventLoop([] {});
 
-	auto result = DialogResult();
+	auto result = PopupResult();
 	auto context = QObject();
 
 	QEventLoop loop;
@@ -39,81 +69,115 @@ DialogResult DefaultDialogHandler(DialogArgs args) {
 		widget = base::make_unique_q<Ui::SeparatePanel>(args.parent);
 		const auto raw = widget.get();
 
-		raw->setTitle(
-			rpl::single(QUrl(QString::fromStdString(args.url)).host()));
+		raw->setTitle(rpl::single(args.title));
 		auto layout = base::make_unique_q<Ui::VerticalLayout>(raw);
 		const auto skip = st::boxDividerHeight;
 		const auto container = layout.get();
 		const auto label = container->add(
 			object_ptr<Ui::FlatLabel>(
 				container,
-				rpl::single(QString::fromStdString(args.text)),
+				rpl::single(args.text),
 				st::boxLabel),
 			st::boxRowPadding);
 		label->resizeToWidth(st::boxWideWidth
 			- st::boxRowPadding.left()
 			- st::boxRowPadding.right());
-		const auto input = (args.type == DialogType::Prompt)
+		const auto input = args.value
 			? container->add(
 				object_ptr<Ui::InputField>(
 					container,
 					st::defaultInputField,
 					rpl::single(QString()),
-					QString::fromStdString(args.value)),
+					*args.value),
 				st::boxRowPadding + QMargins(0, 0, 0, skip))
 			: nullptr;
 		const auto buttonPadding = st::webviewDialogPadding;
-		const auto buttonHeight = buttonPadding.top()
-			+ st::webviewDialogSubmit.height
-			+ buttonPadding.bottom();
 		const auto buttons = container->add(
-			object_ptr<Ui::FixedHeightWidget>(
-				container,
-				st::webviewDialogSubmit.height),
+			object_ptr<Ui::RpWidget>(container),
 			QMargins(
 				buttonPadding.left(),
 				buttonPadding.top(),
 				buttonPadding.left(),
 				buttonPadding.bottom()));
-		const auto submit = Ui::CreateChild<Ui::RoundButton>(
-			buttons,
-			rpl::single(Ui::Integration::Instance().phraseButtonOk()),
-			st::webviewDialogButton);
-		const auto cancel = (args.type != DialogType::Alert)
-			? Ui::CreateChild<Ui::RoundButton>(
+		const auto list = buttons->lifetime().make_state<
+			std::vector<not_null<Ui::RoundButton*>>
+		>();
+		list->reserve(args.buttons.size());
+		for (const auto &descriptor : args.buttons) {
+			using Type = PopupArgs::Button::Type;
+			const auto text = [&] {
+				const auto integration = &Ui::Integration::Instance();
+				switch (descriptor.type) {
+				case Type::Default: return descriptor.text;
+				case Type::Ok: return integration->phraseButtonOk();
+				case Type::Close: return integration->phraseButtonClose();
+				case Type::Cancel: return integration->phraseButtonCancel();
+				case Type::Destructive: return descriptor.text;
+				}
+				Unexpected("Button type in blocking popup.");
+			}();
+			const auto button = Ui::CreateChild<Ui::RoundButton>(
 				buttons,
-				rpl::single(Ui::Integration::Instance().phraseButtonCancel()),
-				st::webviewDialogButton)
-			: nullptr;
+				rpl::single(text),
+				(descriptor.type != Type::Destructive
+					? st::webviewDialogButton
+					: st::webviewDialogDestructiveButton));
+			button->setClickedCallback([=, &result, id = descriptor.id]{
+				result.id = id;
+				if (input) {
+					result.value = input->getLastText();
+				}
+				raw->hideGetDuration();
+			});
+			list->push_back(button);
+		}
+
+		buttons->resizeToWidth(st::boxWideWidth - 2 * buttonPadding.left());
 		buttons->widthValue(
 		) | rpl::start_with_next([=](int width) {
-			submit->moveToRight(0, 0, width);
-			if (cancel) {
-				cancel->moveToRight(
-					submit->width() + st::webviewDialogPadding.right(),
-					0,
-					width);
+			const auto count = list->size();
+			const auto skip = st::webviewDialogPadding.right();
+			const auto vertical = (count > 2)
+				|| (count == 2
+					&& (list->at(0)->width() + skip + list->at(1)->width()
+						> width));
+			const auto single = st::webviewDialogSubmit.height;
+			auto top = 0;
+			auto right = 0;
+			for (const auto &button : *list) {
+				button->moveToRight(right, top, width);
+				if (vertical) {
+					top += single + skip;
+				} else {
+					right += button->width() + skip;
+				}
+			}
+			const auto height = (top > 0) ? (top - skip) : single;
+			if (buttons->height() != height) {
+				buttons->resize(buttons->width(), height);
 			}
 		}, buttons->lifetime());
-		const auto height = st::separatePanelTitleHeight
-			+ label->height()
-			+ (input ? (skip + input->height()) : 0)
-			+ buttonHeight;
-		raw->setInnerSize({ st::boxWideWidth, height });
-		container->resizeToWidth(st::boxWideWidth);
 
-		const auto confirm = [=, &result] {
-			result.accepted = true;
-			if (input) {
-				result.text = input->getLastText().toStdString();
-			}
-			raw->hideGetDuration();
-		};
+		buttons->heightValue(
+		) | rpl::start_with_next([=](int height) {
+			const auto full = st::separatePanelTitleHeight
+				+ label->height()
+				+ (input ? (skip + input->height()) : 0)
+				+ buttonPadding.top()
+				+ height
+				+ buttonPadding.bottom();
+			raw->setInnerSize({ st::boxWideWidth, full });
+		}, buttons->lifetime());
+		container->resizeToWidth(st::boxWideWidth);
 
 		if (input) {
 			input->selectAll();
 			input->setFocusFast();
-			QObject::connect(input, &Ui::InputField::submitted, confirm);
+			const auto submitted = [=, &result] {
+				result.value = input->getLastText();
+				raw->hideGetDuration();
+			};
+			QObject::connect(input, &Ui::InputField::submitted, submitted);
 		}
 		container->events(
 		) | rpl::start_with_next([=](not_null<QEvent*> event) {
@@ -130,14 +194,6 @@ DialogResult DefaultDialogHandler(DialogArgs args) {
 			raw->hideGetDuration();
 		}, raw->lifetime());
 
-		submit->setClickedCallback(confirm);
-
-		if (cancel) {
-			cancel->setClickedCallback([=] {
-				raw->hideGetDuration();
-			});
-		}
-
 		const auto finish = [&] {
 			if (running) {
 				running = false;
@@ -153,6 +209,35 @@ DialogResult DefaultDialogHandler(DialogArgs args) {
 	widget = nullptr;
 
 	return result;
+}
+
+DialogResult DefaultDialogHandler(DialogArgs &&args) {
+	auto buttons = std::vector<PopupArgs::Button>();
+	buttons.push_back({
+		.id = "ok",
+		.type = PopupArgs::Button::Type::Ok,
+	});
+	if (args.type != DialogType::Alert) {
+		buttons.push_back({
+			.id = "cancel",
+			.type = PopupArgs::Button::Type::Cancel,
+		});
+	}
+	const auto result = ShowBlockingPopup({
+		.parent = args.parent,
+		.title = QUrl(QString::fromStdString(args.url)).host(),
+		.text = QString::fromStdString(args.text),
+		.value = (args.type == DialogType::Prompt
+			? QString::fromStdString(args.value)
+			: std::optional<QString>()),
+		.buttons = std::move(buttons),
+	});
+	return {
+		.text = (result.id == "cancel"
+			? std::string()
+			: result.value.value_or(QString()).toStdString()),
+		.accepted = (result.id == "ok" || result.value.has_value()),
+	};
 }
 
 } // namespace Webview
--- webview/webview_dialog.h
+++ webview/webview_dialog.h
@@ -6,13 +6,45 @@
 //
 #pragma once
 
-class QString;
+#include <QtCore/QString>
+
+#include <optional>
+#include <vector>
+
+class QWidget;
 
 namespace Webview {
+
+struct PopupArgs {
+	struct Button {
+		enum class Type {
+			Default,
+			Ok,
+			Close,
+			Cancel,
+			Destructive,
+		};
+		QString id;
+		QString text;
+		Type type = Type::Default;
+	};
+
+	QWidget *parent = nullptr;
+	QString title;
+	QString text;
+	std::optional<QString> value;
+	std::vector<Button> buttons;
+	bool ignoreFloodCheck = false;
+};
+struct PopupResult {
+	std::optional<QString> id;
+	std::optional<QString> value;
+};
+[[nodiscard]] PopupResult ShowBlockingPopup(PopupArgs &&args);
 
 struct DialogArgs;
 struct DialogResult;
 
-[[nodiscard]] DialogResult DefaultDialogHandler(DialogArgs args);
+[[nodiscard]] DialogResult DefaultDialogHandler(DialogArgs &&args);
 
 } // namespace Webview
