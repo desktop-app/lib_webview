2fceb5faf89aa412eb9d8d628ce327e962902916
Ilya Fedin <fedin-ilja2010@ya.ru>
2023-09-27 00:21:11 +0400

    Fix build with Qt 5

--- webview/platform/linux/webview_linux_compositor.cpp
+++ webview/platform/linux/webview_linux_compositor.cpp
@@ -199,8 +199,9 @@ Compositor::Compositor()
 		const auto output = new Output(this, xdgSurface);
 
 		output->chrome().surfaceCompleted() | rpl::start_with_next([=] {
-			const auto parent = qvariant_cast<Output*>(
-				popup->parentXdgSurface()->property("output"))->window();
+			const auto parent = (*static_cast<Output * const *>(
+				popup->parentXdgSurface()->property("output").constData()
+			))->window();
 			if (_private->widget
 					&& parent == _private->widget->quickWindow()) {
 				output->window()->setTransientParent(
