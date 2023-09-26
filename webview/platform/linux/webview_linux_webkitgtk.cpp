25d3e658b0b9a34eef90ea2e2570bd9b0fd47bce
Ilya Fedin <fedin-ilja2010@ya.ru>
2023-09-27 00:21:11 +0400

    Fix build with Qt 5

--- webview/platform/linux/webview_linux_webkitgtk.cpp
+++ webview/platform/linux/webview_linux_webkitgtk.cpp
@@ -33,6 +33,13 @@ constexpr auto kObjectPath = "/org/desktop_app/GtkIntegration/Webview";
 constexpr auto kMasterObjectPath = "/org/desktop_app/GtkIntegration/Webview/Master";
 constexpr auto kHelperObjectPath = "/org/desktop_app/GtkIntegration/Webview/Helper";
 
+void (* const SetGraphicsApi)(QSGRendererInterface::GraphicsApi) =
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+	QQuickWindow::setGraphicsApi;
+#else // Qt >= 6.0.0
+	QQuickWindow::setSceneGraphBackend;
+#endif // Qt < 6.0.0
+
 std::string SocketPath;
 
 inline std::string SocketPathToDBusAddress(const std::string &socketPath) {
@@ -116,12 +123,10 @@ Instance::Instance(bool remoting)
 					Ui::GL::CheckCapabilities(nullptr));
 				switch (backend) {
 				case Ui::GL::Backend::Raster:
-					QQuickWindow::setGraphicsApi(
-						QSGRendererInterface::Software);
+					SetGraphicsApi(QSGRendererInterface::Software);
 					break;
 				case Ui::GL::Backend::OpenGL:
-					QQuickWindow::setGraphicsApi(
-						QSGRendererInterface::OpenGL);
+					SetGraphicsApi(QSGRendererInterface::OpenGL);
 					break;
 				}
 				return true;
