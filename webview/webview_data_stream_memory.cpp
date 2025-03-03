desktop-app/lib_webview

commit b585047fb5cee7e7ac7cb2b017bd6fb46e26a267

Initial instant view implementation for Linux

webview/webview_data_stream_memory.cpp
@@ -6,6 +6,10 @@
 //
 #include "webview/webview_data_stream_memory.h"
 
+#if !defined Q_OS_WIN && !defined Q_OS_MAC
+#include <sys/mman.h>
+#endif // !Q_OS_WIN && !Q_OS_MAC
+
 namespace Webview {
 
 DataStreamFromMemory::DataStreamFromMemory(
@@ -13,6 +17,43 @@ DataStreamFromMemory::DataStreamFromMemory(
 	std::string mime)
 : _data(data)
 , _mime(mime) {
+#if !defined Q_OS_WIN && !defined Q_OS_MAC
+	const auto handle = memfd_create("webview-data-stream", MFD_CLOEXEC);
+	if (handle == -1) {
+		return;
+	}
+	if (ftruncate(handle, data.size()) != 0) {
+		close(handle);
+		return;
+	}
+	const auto shared = mmap(
+		nullptr,
+		data.size(),
+		PROT_READ | PROT_WRITE,
+		MAP_SHARED,
+		handle,
+		0);
+	if (shared == MAP_FAILED) {
+		close(handle);
+		return;
+	}
+	memcpy(shared, data.constData(), data.size());
+	_handle = handle;
+	_data.setRawData((char*)shared, data.size());
+#endif // !Q_OS_WIN && !Q_OS_MAC
+}
+
+DataStreamFromMemory::~DataStreamFromMemory() {
+#if !defined Q_OS_WIN && !defined Q_OS_MAC
+	if (_handle) {
+		munmap((void*)_data.constData(), _data.size());
+		close(_handle);
+	}
+#endif // !Q_OS_WIN && !Q_OS_MAC
+}
+
+int DataStreamFromMemory::handle() {
+	return _handle;
 }
 
 std::int64_t DataStreamFromMemory::size() {

webview/platform/linux/webview_linux.cpp
@@ -20,7 +20,7 @@ bool SupportsEmbedAfterCreate() {
 }
 
 bool NavigateToDataSupported() {
-	return false;
+	return WebKitGTK::NavigateToDataSupported();
 }
 
 bool SeparateStorageIdSupported() {

webview/platform/linux/webview_linux_interface.xml
@@ -28,6 +28,13 @@
 			<arg type='b' name='canGoBack' direction='in'/>
 			<arg type='b' name='canGoForward' direction='in'/>
 		</method>
+		<method name='DataRequest'>
+			<arg type='t' name='request' direction='in'/>
+			<arg type='s' name='id' direction='in'/>
+			<arg type='x' name='offset' direction='in'/>
+			<arg type='x' name='limit' direction='in'/>
+			<arg type='i' name='result' direction='out'/>
+		</method>
 	</interface>
 	<interface name='org.desktop_app.GtkIntegration.Webview.Helper'>
 		<method name='Create'>
@@ -36,6 +43,7 @@
 			<arg type='i' name='g' direction='in'/>
 			<arg type='i' name='b' direction='in'/>
 			<arg type='i' name='a' direction='in'/>
+			<arg type='s' name='protocol' direction='in'/>
 			<arg type='s' name='path' direction='in'/>
 		</method>
 		<method name='Reload'/>
@@ -59,6 +67,14 @@
 		</method>
 		<method name='GetWinId'>
 			<arg type='t' name='result' direction='out'/>
+		</method>
+		<method name='DataResponse'>
+			<annotation name="org.gtk.GDBus.C.UnixFD" value="true"/>
+			<arg type='t' name='request' direction='in'/>
+			<arg type='h' name='fd' direction='in'/>
+			<arg type='x' name='offset' direction='in'/>
+			<arg type='x' name='size' direction='in'/>
+			<arg type='s' name='mime' direction='in'/>
 		</method>
 		<signal name='Started'/>
 	</interface>

webview/platform/linux/webview_linux_webkitgtk.cpp
@@ -8,6 +8,7 @@
 
 #include "webview/platform/linux/webview_linux_webkitgtk_library.h"
 #include "webview/platform/linux/webview_linux_compositor.h"
+#include "webview/webview_data_stream.h"
 #include "base/platform/base_platform_info.h"
 #include "base/debug_log.h"
 #include "base/integration.h"
@@ -30,6 +31,7 @@
 #include <crl/crl.h>
 #include <rpl/rpl.h>
 #include <regex>
+#include <sys/mman.h>
 
 namespace Webview::WebKitGTK {
 namespace {
@@ -42,6 +44,8 @@ namespace GObject = gi::repository::GObject;
 constexpr auto kObjectPath = "/org/desktop_app/GtkIntegration/Webview";
 constexpr auto kMasterObjectPath = "/org/desktop_app/GtkIntegration/Webview/Master";
 constexpr auto kHelperObjectPath = "/org/desktop_app/GtkIntegration/Webview/Helper";
+constexpr auto kDataUrlScheme = "desktop-app-resource";
+constexpr auto kFullDomain = "desktop-app-resource://domain/";
 
 std::string SocketPath;
 
@@ -50,6 +54,13 @@ inline auto MethodError() {
 		Gio::DBusErrorNS_::quark(),
 		int(Gio::DBusError::UNKNOWN_METHOD_),
 		"Method does not exist.");
+}
+
+inline auto NotFoundError() {
+	return GLib::Error::new_literal(
+		G_IO_ERROR,
+		G_IO_ERROR_NOT_FOUND,
+		"Not Found");
 }
 
 inline std::string SocketPathToDBusAddress(const std::string &socketPath) {
@@ -100,6 +111,14 @@ private:
 	GtkWidget *createAnother(WebKitNavigationAction *action);
 	bool scriptDialog(WebKitScriptDialog *dialog);
 
+	void dataRequest(WebKitURISchemeRequest *request);
+	void dataResponse(
+		WebKitURISchemeRequest *request,
+		int fd,
+		int64 offset,
+		int64 size,
+		std::string mime);
+
 	void startProcess();
 	void stopProcess();
 	void updateHistoryStates();
@@ -132,6 +151,9 @@ private:
 	std::function<void(bool)> _navigationDoneHandler;
 	std::function<DialogResult(DialogArgs)> _dialogHandler;
 	rpl::variable<NavigationHistoryState> _navigationHistoryState;
+	std::function<DataResult(DataRequest)> _dataRequestHandler;
+	std::string _dataProtocol;
+	std::string _dataDomain;
 	bool _loadFailed = false;
 
 };
@@ -176,6 +198,13 @@ bool Instance::create(Config config) {
 	_navigationStartHandler = std::move(config.navigationStartHandler);
 	_navigationDoneHandler = std::move(config.navigationDoneHandler);
 	_dialogHandler = std::move(config.dialogHandler);
+	_dataRequestHandler = std::move(config.dataRequestHandler);
+	_dataProtocol = kDataUrlScheme;
+	_dataDomain = kFullDomain;
+	if (!config.dataProtocolOverride.empty()) {
+		_dataProtocol = config.dataProtocolOverride;
+		_dataDomain = _dataProtocol + "://domain/";
+	}
 
 	if (_remoting) {
 		const auto resolveResult = resolve();
@@ -225,8 +254,9 @@ bool Instance::create(Config config) {
 		const auto g = config.opaqueBg.green();
 		const auto b = config.opaqueBg.blue();
 		const auto a = config.opaqueBg.alpha();
+		const auto protocol = config.dataProtocolOverride;
 		const auto path = config.userDataPath;
-		_helper.call_create(debug, r, g, b, a, path, crl::guard(&guard, [&](
+		_helper.call_create(debug, r, g, b, a, protocol, path, crl::guard(&guard, [&](
 				GObject::Object source_object,
 				Gio::AsyncResult res) {
 			success = _helper.call_create_finish(res, nullptr);
@@ -292,22 +322,27 @@ bool Instance::create(Config config) {
 	const auto baseCache = base + "/cache";
 	const auto baseData = base + "/data";
 
+	WebKitWebContext *context = nullptr;
 	if (webkit_network_session_new) {
+		context = webkit_web_context_new();
 		WebKitNetworkSession *session = webkit_network_session_new(
 			baseData.c_str(),
 			baseCache.c_str());
 		_webview = WEBKIT_WEB_VIEW(g_object_new(
 			WEBKIT_TYPE_WEB_VIEW,
+			"web-context",
+			context,
 			"network-session",
 			session,
 			nullptr));
 		g_object_unref(session);
+		g_object_unref(context);
 	} else {
 		WebKitWebsiteDataManager *data = webkit_website_data_manager_new(
 			"base-cache-directory", baseCache.c_str(),
 			"base-data-directory", baseData.c_str(),
 			nullptr);
-		WebKitWebContext *context = webkit_web_context_new_with_website_data_manager(data);
+		context = webkit_web_context_new_with_website_data_manager(data);
 		g_object_unref(data);
 
 		_webview = WEBKIT_WEB_VIEW(webkit_web_view_new_with_context(context));
@@ -417,6 +452,16 @@ bool Instance::create(Config config) {
 	webkit_user_content_manager_register_script_message_handler(
 		manager,
 		"external",
+		nullptr);
+	webkit_web_context_register_uri_scheme(
+		context,
+		_dataProtocol.c_str(),
+		WebKitURISchemeRequestCallback(+[](
+			WebKitURISchemeRequest *request,
+			Instance *instance) {
+			instance->dataRequest(request);
+		}),
+		this,
 		nullptr);
 	const GdkRGBA rgba{ 0.f, 0.f, 0.f, 0.f, };
 	webkit_web_view_set_background_color(_webview, &rgba);
@@ -567,6 +612,71 @@ bool Instance::scriptDialog(WebKitScriptDialog *dialog) {
 	return true;
 }
 
+void Instance::dataRequest(WebKitURISchemeRequest *request) {
+	if (!_master) {
+		webkit_uri_scheme_request_finish_error(
+			request,
+			NotFoundError().gobj_());
+		return;
+	}
+	g_object_ref(request);
+	_master.call_data_request(
+		uintptr_t(request),
+		webkit_uri_scheme_request_get_path(request) + 1,
+		0,
+		0,
+		[=](GObject::Object source_object, Gio::AsyncResult res) {
+			const auto ret = _master.call_data_request_finish(res);
+			if (!ret || DataResult(std::get<1>(*ret)) == DataResult::Failed) {
+				webkit_uri_scheme_request_finish_error(
+					request,
+					NotFoundError().gobj_());
+				g_object_unref(request);
+			}
+		});
+}
+
+void Instance::dataResponse(
+		WebKitURISchemeRequest *request,
+		int fd,
+		int64 offset,
+		int64 size,
+		std::string mime) {
+	const auto data = mmap(
+		nullptr,
+		offset + size,
+		PROT_READ,
+		MAP_PRIVATE,
+		fd,
+		0);
+
+	if (data == MAP_FAILED) {
+		webkit_uri_scheme_request_finish_error(
+			request,
+			NotFoundError().gobj_());
+		g_object_unref(request);
+		close(fd);
+		return;
+	}
+
+	const auto stream = Gio::MemoryInputStream::new_from_bytes(
+		GLib::Bytes::new_with_free_func(
+			reinterpret_cast<const uchar*>(data) + offset,
+			size,
+			[=] { munmap(data, offset + size); }));
+
+	const auto response = webkit_uri_scheme_response_new(
+		G_INPUT_STREAM(stream.gobj_()),
+		size);
+
+	webkit_uri_scheme_response_set_content_type(response, mime.c_str());
+	webkit_uri_scheme_request_finish_with_response(request, response);
+
+	g_object_unref(response);
+	g_object_unref(request);
+	close(fd);
+}
+
 ResolveResult Instance::resolve() {
 	if (_remoting) {
 		if (!_helper) {
@@ -609,7 +719,7 @@ void Instance::navigate(std::string url) {
 }
 
 void Instance::navigateToData(std::string id) {
-	Unexpected("WebKitGTK::Instance::navigateToData.");
+	navigate(_dataDomain + id);
 }
 
 void Instance::reload() {
@@ -960,20 +1070,18 @@ void Instance::registerMasterMethodHandlers() {
 			Gio::DBusMethodInvocation invocation,
 			const std::string &uri,
 			bool newWindow) {
-		if (!_navigationStartHandler) {
-			invocation.return_gerror(MethodError());
-			return true;
-		}
-
-		_master.complete_navigation_started(invocation, [&] {
-			if (newWindow) {
-				if (_navigationStartHandler(uri, true)) {
-					QDesktopServices::openUrl(QString::fromStdString(uri));
-				}
-				return false;
+		if (newWindow) {
+			if (_navigationStartHandler && _navigationStartHandler(uri, true)) {
+				QDesktopServices::openUrl(QString::fromStdString(uri));
 			}
-			return _navigationStartHandler(uri, false);
-		}());
+			_master.complete_navigation_started(invocation, false);
+		} else if (!std::string(uri).starts_with(_dataDomain)
+				&& _navigationStartHandler
+				&& !_navigationStartHandler(uri, false)) {
+			_master.complete_navigation_started(invocation, false);
+		} else {
+			_master.complete_navigation_started(invocation, true);
+		}
 
 		return true;
 	});
@@ -1035,6 +1143,51 @@ void Instance::registerMasterMethodHandlers() {
 			.canGoBack = canGoBack,
 			.canGoForward = canGoForward,
 		};
+		return true;
+	});
+
+	_master.signal_handle_data_request().connect([=](
+			Master,
+			Gio::DBusMethodInvocation invocation,
+			uint64 req,
+			const std::string &id,
+			int64 offset,
+			int64 limit) {
+		if (!_dataRequestHandler) {
+			invocation.return_gerror(MethodError());
+			return true;
+		}
+
+		_master.complete_data_request(
+			invocation,
+			int(_dataRequestHandler(DataRequest{
+				.id = id,
+				.offset = offset,
+				.limit = limit,
+				.done = crl::guard(this, [=](DataResponse resolved) {
+					const auto request = reinterpret_cast<
+						WebKitURISchemeRequest*
+					>(uintptr_t(req));
+					auto &stream = resolved.stream;
+					const auto fd = stream ? dup(stream->handle()) : -1;
+					if (!_helper || !stream || fd == -1) {
+						webkit_uri_scheme_request_finish_error(
+							request,
+							NotFoundError().gobj_());
+						g_object_unref(request);
+					}
+					_helper.call_data_response(
+						req,
+						GLib::Variant::new_handle(0),
+						resolved.streamOffset,
+						resolved.totalSize ?: stream->size(),
+						stream->mime(),
+						Gio::UnixFDList::new_from_array(&fd, 1),
+						nullptr,
+						nullptr);
+				}),
+			})));
+
 		return true;
 	});
 }
@@ -1171,9 +1324,11 @@ void Instance::registerHelperMethodHandlers() {
 			int g,
 			int b,
 			int a,
+			const std::string &protocol,
 			const std::string &path) {
 		if (create({
 			.opaqueBg = QColor(r, g, b, a),
+			.dataProtocolOverride = protocol,
 			.userDataPath = path,
 			.debug = debug,
 		})) {
@@ -1246,6 +1401,22 @@ void Instance::registerHelperMethodHandlers() {
 			reinterpret_cast<uint64>(winId()));
 		return true;
 	});
+
+	_helper.signal_handle_data_response().connect([=](
+			Helper,
+			Gio::DBusMethodInvocation invocation,
+			Gio::UnixFDList fds,
+			uint64 req,
+			GLib::Variant fd,
+			int64 offset,
+			int64 size,
+			const std::string &mime) {
+		const auto request = (WebKitURISchemeRequest*)uintptr_t(req);
+		const auto handle = fds.get(fd.get_handle(), nullptr);
+		dataResponse(request, handle, offset, size, mime);
+		_helper.complete_data_response(invocation);
+		return true;
+	});
 }
 
 } // namespace
@@ -1277,6 +1448,11 @@ Available Availability() {
 		};
 	}
 	return Available{};
+}
+
+bool NavigateToDataSupported() {
+	return Instance().resolve() == ResolveResult::Success;
+	return false;
 }
 
 std::unique_ptr<Interface> CreateInstance(Config config) {

webview/platform/linux/webview_linux_webkitgtk.h
@@ -11,6 +11,7 @@
 namespace Webview::WebKitGTK {
 
 [[nodiscard]] Available Availability();
+[[nodiscard]] bool NavigateToDataSupported();
 [[nodiscard]] std::unique_ptr<Interface> CreateInstance(Config config);
 
 int Exec();

webview/platform/linux/webview_linux_webkitgtk_library.cpp
@@ -43,6 +43,11 @@ ResolveResult Resolve(bool wayland) {
 			|| (LOAD_LIBRARY_SYMBOL(lib, gtk_plug_new)
 				&& LOAD_LIBRARY_SYMBOL(lib, gtk_plug_get_id)
 				&& LOAD_LIBRARY_SYMBOL(lib, gtk_plug_get_type)))
+		&& LOAD_LIBRARY_SYMBOL(lib, soup_message_headers_new)
+		&& LOAD_LIBRARY_SYMBOL(lib, soup_message_headers_append)
+		&& LOAD_LIBRARY_SYMBOL(lib, soup_message_headers_get_one)
+		&& (LOAD_LIBRARY_SYMBOL(lib, soup_message_headers_unref)
+			|| LOAD_LIBRARY_SYMBOL(lib, soup_message_headers_free))
 		&& LOAD_LIBRARY_SYMBOL(lib, jsc_value_to_string)
 		&& LOAD_LIBRARY_SYMBOL(lib, webkit_navigation_policy_decision_get_type)
 		&& LOAD_LIBRARY_SYMBOL(lib, webkit_navigation_policy_decision_get_navigation_action)
@@ -72,10 +77,20 @@ ResolveResult Resolve(bool wayland) {
 		&& (LOAD_LIBRARY_SYMBOL(lib, webkit_web_view_evaluate_javascript)
 			|| LOAD_LIBRARY_SYMBOL(lib, webkit_web_view_run_javascript))
 		&& LOAD_LIBRARY_SYMBOL(lib, webkit_web_view_set_background_color)
-		&& (LOAD_LIBRARY_SYMBOL(lib, webkit_network_session_new)
+		&& ((LOAD_LIBRARY_SYMBOL(lib, webkit_network_session_new)
+				&& LOAD_LIBRARY_SYMBOL(lib, webkit_web_context_new))
 			|| (LOAD_LIBRARY_SYMBOL(lib, webkit_web_view_new_with_context)
 				&& LOAD_LIBRARY_SYMBOL(lib, webkit_website_data_manager_new)
-				&& LOAD_LIBRARY_SYMBOL(lib, webkit_web_context_new_with_website_data_manager)));
+				&& LOAD_LIBRARY_SYMBOL(lib, webkit_web_context_new_with_website_data_manager)))
+		&& LOAD_LIBRARY_SYMBOL(lib, webkit_web_context_register_uri_scheme)
+		&& LOAD_LIBRARY_SYMBOL(lib, webkit_uri_scheme_request_get_path)
+		&& LOAD_LIBRARY_SYMBOL(lib, webkit_uri_scheme_request_finish_error)
+		&& LOAD_LIBRARY_SYMBOL(lib, webkit_uri_scheme_request_finish_with_response)
+		&& LOAD_LIBRARY_SYMBOL(lib, webkit_uri_scheme_request_get_http_headers)
+		&& LOAD_LIBRARY_SYMBOL(lib, webkit_uri_scheme_response_new)
+		&& LOAD_LIBRARY_SYMBOL(lib, webkit_uri_scheme_response_set_content_type)
+		&& LOAD_LIBRARY_SYMBOL(lib, webkit_uri_scheme_response_set_http_headers)
+		&& LOAD_LIBRARY_SYMBOL(lib, webkit_uri_scheme_response_set_status);
 	if (!result) {
 		return ResolveResult::NoLibrary;
 	}

webview/platform/linux/webview_linux_webkitgtk_library.h
@@ -49,6 +49,8 @@ typedef struct _GtkStyleContext GtkStyleContext;
 typedef struct _GtkStyleProvider GtkStyleProvider;
 typedef struct _GtkCssProvider GtkCssProvider;
 
+typedef struct _SoupMessageHeaders SoupMessageHeaders;
+
 typedef struct _JSCValue JSCValue;
 
 typedef struct _WebKitJavascriptResult WebKitJavascriptResult;
@@ -56,6 +58,8 @@ typedef struct _WebKitNavigationAction WebKitNavigationAction;
 typedef struct _WebKitNavigationPolicyDecision WebKitNavigationPolicyDecision;
 typedef struct _WebKitPolicyDecision WebKitPolicyDecision;
 typedef struct _WebKitURIRequest WebKitURIRequest;
+typedef struct _WebKitURISchemeRequest WebKitURISchemeRequest;
+typedef struct _WebKitURISchemeResponse WebKitURISchemeResponse;
 typedef struct _WebKitUserContentManager WebKitUserContentManager;
 typedef struct _WebKitUserScript WebKitUserScript;
 typedef struct _WebKitWebView WebKitWebView;
@@ -75,6 +79,12 @@ typedef enum {
 	WEBKIT_WEB_PROCESS_EXCEEDED_MEMORY_LIMIT,
 	WEBKIT_WEB_PROCESS_TERMINATED_BY_API,
 } WebKitWebProcessTerminationReason;
+
+typedef enum {
+	SOUP_MESSAGE_HEADERS_REQUEST,
+	SOUP_MESSAGE_HEADERS_RESPONSE,
+	SOUP_MESSAGE_HEADERS_MULTIPART,
+} SoupMessageHeadersType;
 
 typedef enum {
 	WEBKIT_LOAD_STARTED,
@@ -105,6 +115,10 @@ typedef enum {
     WEBKIT_SCRIPT_DIALOG_PROMPT,
     WEBKIT_SCRIPT_DIALOG_BEFORE_UNLOAD_CONFIRM,
 } WebKitScriptDialogType;
+
+typedef void (*WebKitURISchemeRequestCallback)(
+	WebKitURISchemeRequest *request,
+	gpointer user_data);
 
 namespace Webview::WebKitGTK::Library {
 
@@ -161,6 +175,18 @@ inline void (*gtk_css_provider_load_from_data)(
 inline GtkWidget *(*gtk_plug_new)(unsigned long socket_id);
 inline unsigned long (*gtk_plug_get_id)(GtkPlug *plug);
 inline GType (*gtk_plug_get_type)(void);
+
+inline SoupMessageHeaders *(*soup_message_headers_new)(
+	SoupMessageHeadersType type);
+inline void (*soup_message_headers_append)(
+	SoupMessageHeaders *hdrs,
+	const char *name,
+	const char *value);
+inline const char *(*soup_message_headers_get_one)(
+	SoupMessageHeaders *hdrs,
+	const char *name);
+inline void (*soup_message_headers_unref)(SoupMessageHeaders *hdrs);
+inline void (*soup_message_headers_free)(SoupMessageHeaders *hdrs);
 
 inline char *(*jsc_value_to_string)(JSCValue *value);
 inline JSCValue *(*webkit_javascript_result_get_js_value)(
@@ -241,11 +267,41 @@ inline void (*webkit_web_view_set_background_color)(
 inline WebKitWebsiteDataManager *(*webkit_website_data_manager_new)(
 	const gchar *first_option_name,
 	...);
+inline WebKitWebContext *(*webkit_web_context_new)(void);
 inline WebKitWebContext *(*webkit_web_context_new_with_website_data_manager)(
 	WebKitWebsiteDataManager* manager);
+inline void (*webkit_web_context_register_uri_scheme)(
+	WebKitWebContext *context,
+	const gchar *scheme,
+	WebKitURISchemeRequestCallback callback,
+	gpointer user_data,
+	GDestroyNotify user_data_destroy_func);
 inline WebKitNetworkSession *(*webkit_network_session_new)(
 	const char* data_directory,
 	const char* cache_directory);
+inline const gchar *(*webkit_uri_scheme_request_get_path)(
+	WebKitURISchemeRequest *request);
+inline void (*webkit_uri_scheme_request_finish_error)(
+	WebKitURISchemeRequest *request,
+	GError *error);
+inline void (*webkit_uri_scheme_request_finish_with_response)(
+	WebKitURISchemeRequest *request,
+	WebKitURISchemeResponse *response);
+inline SoupMessageHeaders *(*webkit_uri_scheme_request_get_http_headers)(
+	WebKitURISchemeRequest *request);
+inline WebKitURISchemeResponse *(*webkit_uri_scheme_response_new)(
+	GInputStream *stream,
+	gint64 stream_length);
+inline void (*webkit_uri_scheme_response_set_content_type)(
+	WebKitURISchemeResponse *response,
+	const gchar *content_type);
+inline void (*webkit_uri_scheme_response_set_http_headers)(
+	WebKitURISchemeResponse *response,
+	SoupMessageHeaders *headers);
+inline void (*webkit_uri_scheme_response_set_status)(
+	WebKitURISchemeResponse *response,
+	guint status_code,
+	const gchar *reason_phrase);
 
 enum class ResolveResult {
 	Success,

webview/webview_data_stream.h
@@ -15,6 +15,7 @@ class DataStream {
 public:
 	virtual ~DataStream() = default;
 
+	[[nodiscard]] virtual int handle() = 0;
 	[[nodiscard]] virtual std::int64_t size() = 0;
 	[[nodiscard]] virtual std::string mime() = 0;
 

commit 5c98e563c10bb5622711d809b968df1c47477603

Add missing include of unistd.h on Linux
Functions like ftruncate and close are defined in unistd.h. The header
should be included before their usage.

This fixes building on libc++.

Fixes: b585047 ("Initial instant view implementation for Linux")
Signed-off-by: Yao Zi <ziyao@disroot.org>

webview/webview_data_stream_memory.cpp
@@ -7,6 +7,7 @@
 #include "webview/webview_data_stream_memory.h"
 
 #if !defined Q_OS_WIN && !defined Q_OS_MAC
+#include <unistd.h>
 #include <sys/mman.h>
 #endif // !Q_OS_WIN && !Q_OS_MAC
 